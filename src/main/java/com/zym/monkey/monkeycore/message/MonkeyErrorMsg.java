eaad62a7326e81aaf0811da71c1a14f48d35caf6
liangziqiang <liangziqiang@seemmo.com>
2018-05-22 09:10:56 +0800

    组件（过滤器）报错信息

---
+++ src/main/java/com/zym/monkey/monkeycore/message/MonkeyErrorMsg.java
@@ -0,0 +1,21 @@
+package com.zym.monkey.monkeycore.message;
+
+/**
+ * @author 梁自强
+ * @date 2018/5/21 16:39
+ * @desc 组件处理请求时的报错信息
+ */
+public class MonkeyErrorMsg {
+    /*
+    * 还没想好
+    */
+    private String errContent;
+
+    public String getErrContent() {
+        return errContent;
+    }
+
+    public void setErrContent(String errContent) {
+        this.errContent = errContent;
+    }
+}
