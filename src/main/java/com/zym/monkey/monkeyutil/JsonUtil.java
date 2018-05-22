efceb6ed75b58ea9428285a8b0eaa8aa00d98f7d
liangziqiang <liangziqiang@seemmo.com>
2018-05-22 09:15:23 +0800

    对JSON和POJO 之间的转换工具类 ，只是在Gson 外面包了一层，不算是造轮子

---
+++ src/main/java/com/zym/monkey/monkeyutil/JsonUtil.java
@@ -0,0 +1,15 @@
+package com.zym.monkey.monkeyutil;
+
+import com.google.gson.Gson;
+
+/**
+ * @author 梁自强
+ * @date 2018/5/21 16:45
+ * @desc json 转换 工具类 封装成为静态方法好可以有效的调用
+ */
+public class JsonUtil {
+    private static Gson gson = new Gson();
+    public static String toJsonString(Object obj) {
+       return new Gson().toJson(obj);
+    }
+}
