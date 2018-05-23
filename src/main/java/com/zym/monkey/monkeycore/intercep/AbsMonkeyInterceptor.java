e8c0ad18e0534926775e9f4c299bb52b741fdc24
liangziqiang <liangziqiang@seemmo.com>
2018-05-23 18:27:48 +0800

    实现Interceptor接口，添加两个方法：intercepterBefore和interceptorAfter

--- src/main/java/com/zym/monkey/monkeycore/intercep/AbsMonkeyInterceptor.java
+++ src/main/java/com/zym/monkey/monkeycore/intercep/AbsMonkeyInterceptor.java
@@ -1,5 +1,6 @@
 package com.zym.monkey.monkeycore.intercep;
 
+import com.zym.monkey.monkeycore.message.ResponseMessage;
 import io.netty.handler.codec.http.FullHttpRequest;
 
 /**
@@ -7,16 +8,22 @@ import io.netty.handler.codec.http.FullHttpRequest;
  * @date 2018/5/22 16:26
  * @desc
  */
-public class AbsMonkeyInterceptor implements Interceptor{
+public abstract class AbsMonkeyInterceptor implements Interceptor {
     @Override
     public Object intercept(MethodInvocation methodInvocation) {
+        Object result = null;
         Object[] parameters = methodInvocation.getParameters();
-        interceptBefore((FullHttpRequest)parameters[0]);
-        return null;
-    }
-
-    public void interceptBefore(FullHttpRequest request) {
-
+        FullHttpRequest request = (FullHttpRequest) parameters[0];
+        ResponseMessage responseMessage = (ResponseMessage) parameters[1];
+        interceptBefore(request);
+        try {
+            result = methodInvocation.executeNext();
+            interceptAfter(request, responseMessage);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
