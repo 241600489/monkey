44cde17f3bef7e7f303e02fb4eaec449592a8fe9
liangziqiang <liangziqiang@seemmo.com>
2018-05-25 17:26:02 +0800

    调整代码结构

--- src/main/java/com/zym/monkey/monkeycore/intercep/InterceptorChain.java
+++ src/main/java/com/zym/monkey/monkeycore/intercep/InterceptorChain.java
@@ -1,7 +1,8 @@
 package com.zym.monkey.monkeycore.intercep;
 
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -13,16 +14,15 @@ public class InterceptorChain {
     //由于这是个有状态的，所以采用ConcurrentHashMap 做并发控制
     private Map<String, Interceptor> interceptorMap = new ConcurrentHashMap<>();
 
-
-
-    public void addInterceptor(String name,Interceptor interceptor) {
+    public void addInterceptor(String name, Interceptor interceptor) {
         this.interceptorMap.put(name, interceptor);
     }
 
     public Interceptor removeInterceptor(String name) {
         return this.interceptorMap.remove(name);
     }
-    public List<Interceptor> interceptors(){
+
+    public List<Interceptor> interceptors() {
         return new ArrayList<Interceptor>(this.interceptorMap.values());
     }
 
