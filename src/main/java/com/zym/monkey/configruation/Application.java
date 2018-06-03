65e1108abf8ec0a7de818ae4390d3664c7546c80
liangziqiang <liangziqiang@feellike21.com>
2018-06-03 08:59:06 +0800

    项目的默认配置

---
+++ src/main/java/com/zym/monkey/configruation/Application.java
@@ -0,0 +1,16 @@
+package com.zym.monkey.configruation;
+
+/**
+ * @Author 梁自强
+ * @date 2018/6/3 0003 8:55
+ * @desc 项目的基本配置
+ */
+public class Application {
+    /**
+     * 项目的指定监听端口
+     */
+    private String port;
+    /**
+     * 项目的指定线程数
+     */
+}
